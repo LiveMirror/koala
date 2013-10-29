87e11e546bb6bbde7c8df4c708bc97a4dfdb1366
lingen.liu <lingen.liu@gmail.com>
2013-10-29 15:03:50 +0800

    不正确的参数命名修正

--- koala-bpm/koala-jbpm-core/koala-jbpm-core-application/src/main/java/org/openkoala/jbpm/application/vo/KoalaBPMVariable.java
+++ koala-bpm/koala-jbpm-core/koala-jbpm-core-application/src/main/java/org/openkoala/jbpm/application/vo/KoalaBPMVariable.java
@@ -10,25 +10,25 @@ public class KoalaBPMVariable {
 	/**
 	 * 流程创建者
 	 */
-	public static final String CREATE_USER = "~KJ_USER";
+	public static final String CREATE_USER = "KJ_USER";
 	
 	/**
 	 * 节点处理人
 	 */
-	public static final String NODE_USER = "~KJ_NODE_USER";
+	public static final String NODE_USER = "KJ_NODE_USER";
 	
 	/**
 	 * 忽略日志节点
 	 */
-	public static final String INGORE_LOG = "~KJ_INGORE_LOG";
+	public static final String INGORE_LOG = "KJ_INGORE_LOG";
 	
 	/**
 	 *  审批结果
 	 */
-	public static final String COMMENT = "~KJ_COMMENT";
+	public static final String COMMENT = "KJ_COMMENT";
 	
 	/**
 	 * 审批意见
 	 */
-	public static final String RESULT = "~KJ_RESULT";
+	public static final String RESULT = "KJ_RESULT";
 }
