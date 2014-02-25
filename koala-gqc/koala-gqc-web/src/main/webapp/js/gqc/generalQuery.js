37c9d7d6fb2130b8bd32431d030bf73ba53f3962
jianbing <jianbing@wangjianbing>
2014-02-25 14:52:05 +0800

    修改BUG

--- koala-gqc/koala-gqc-web/src/main/webapp/js/gqc/generalQuery.js
+++ koala-gqc/koala-gqc-web/src/main/webapp/js/gqc/generalQuery.js
@@ -542,7 +542,8 @@ var generalQuery = function(){
 					content: "数据源不可用！"
 				});
 			} else if (data.result == "该数据源可用") {
-				window.open(contextPath + '/pages/gqc/previewTemplate.jsp?id='+id,'预览');
+				var previewWindow = window.open(contextPath + '/pages/gqc/previewTemplate.jsp?id='+id, '预览');
+				previewWindow.resizeTo(previewWindow.screen.width, previewWindow.screen.height);
 			} else {
 				$('#generalQueryGrid').message({
 					type: 'error',
