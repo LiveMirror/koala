710f750f733859930d545718263a2e8be2827172
mluo <luomin@foreveross.com>
2014-02-25 12:51:12 +0800

    解决git冲突

--- koala-opencis/koala-openci-platform/koala-openci-platform-application/src/main/java/org/openkoala/openci/pojo/GitConfigurationPojo.java
+++ koala-opencis/koala-openci-platform/koala-openci-platform-application/src/main/java/org/openkoala/openci/pojo/GitConfigurationPojo.java
@@ -11,11 +11,7 @@ public class GitConfigurationPojo extends ToolConfigurationPojo {
 	@Override
 	public void createCISClient(ToolConfiguration toolConfiguration) {
 		if (toolConfiguration instanceof GitConfiguration) {
-<<<<<<< HEAD
 			GitlabClient gitlabCISClient = new GitlabClient(createGitlabConfiguration((GitConfiguration) toolConfiguration));
-=======
-			GitlabClient gitlabCISClient = new GitlabClient(createGitlabConfiguration((GitConfiguration)toolConfiguration));
->>>>>>> 7738016eef20d78c8ef677fb422a01dbc00e2054
 			cisClient = gitlabCISClient;
 			isInstance = true;
 		}
