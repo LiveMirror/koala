b039f6b5f1241072de404de3a49e0da0832558d4
zacker <zacker330@gmail.com>
2014-02-26 09:51:48 +0800

    加入新的业务日志系统的测试

---
+++ koala-businesslog/koala-businesslog-sample2/src/test/java/MainIntegrationTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * User: zjzhai
+ * Date: 2/26/14
+ * Time: 9:44 AM
+ */
+public class MainIntegrationTest {
+    @Test
+    public void testName() throws Exception {
+
+
+    }
+}
