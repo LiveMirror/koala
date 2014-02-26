30e049c428d2a325fc107abdda8cdeaf782a4e12
jianbing <jianbing@wangjianbing>
2014-02-26 10:19:47 +0800

    修改表格组件

--- koala-businesslog/koala-businesslog-sample/src/main/webapp/lib/koala-ui.plugin.js
+++ koala-businesslog/koala-businesslog-sample/src/main/webapp/lib/koala-ui.plugin.js
@@ -299,10 +299,12 @@
 					self.items = result.Rows;
 					self.totalRecord = result.Total;
 					if(result.Rows.length == 0){
+						self.pages.hide();
 						self.gridTableBodyTable.empty();
 						self.gridTableBody.find('[data-role="noData"]').remove();
 						self.gridTableBody.append($('<div data-role="noData" style="font-size:16px ; padding: 20px; width:'+self.gridTableBodyTable.width()+'px;">'+self.options.noDataText+'</div>'));
 					}else{
+						self.pages.show();
 						self.gridTableBody.find('[data-role="noData"]').remove();
 						self.renderDatas();
 					}
