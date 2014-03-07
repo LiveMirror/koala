c8c2cf234eaff5153b6339b22cab322a37a71e37
zhaoyuxin <zhaoyuxin@foreveross.com>
2014-03-07 09:40:16 +0800

    优化查询组织架构的代码

--- koala-organisation/koala-organisation-applicationImpl/src/main/java/org/openkoala/organisation/application/impl/OrganizationApplicationImpl.java
+++ koala-organisation/koala-organisation-applicationImpl/src/main/java/org/openkoala/organisation/application/impl/OrganizationApplicationImpl.java
@@ -1,7 +1,9 @@
 package org.openkoala.organisation.application.impl;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.ejb.Remote;
@@ -9,6 +11,8 @@ import javax.ejb.Stateless;
 import javax.inject.Named;
 import javax.interceptor.Interceptors;
 
+import org.dayatang.domain.InstanceFactory;
+import org.dayatang.querychannel.QueryChannelService;
 import org.openkoala.organisation.application.OrganizationApplication;
 import org.openkoala.organisation.application.dto.OrganizationDTO;
 import org.openkoala.organisation.domain.Company;
@@ -22,19 +26,19 @@ import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 组织机构应用实现层类
- *
+ * 
  */
 @Named
-@Transactional(value="transactionManager_org")
+@Transactional(value = "transactionManager_org")
 @Interceptors(value = org.openkoala.koala.util.SpringEJBIntercepter.class)
 @Stateless(name = "OrganizationApplication")
 @Remote
 public class OrganizationApplicationImpl implements OrganizationApplication {
-	
-	public boolean isTopOrganizationExists(){
-		return Organization.getTopOrganization()==null?false:true;
+
+	public boolean isTopOrganizationExists() {
+		return Organization.getTopOrganization() == null ? false : true;
 	}
-	
+
 	@Override
 	public void createAsTopOrganization(Company company) {
 		company.createAsTopOrganization();
@@ -62,14 +66,17 @@ public class OrganizationApplicationImpl implements OrganizationApplication {
 
 	@Override
 	public OrganizationDTO getOrganizationTree() {
-		Organization organization = Organization.getTopOrganization();
-		OrganizationDTO top = OrganizationDTO.generateDtoBy(organization);
-		findChildrenOfOrganization(top, organization);
+		// Organization organization = Organization.getTopOrganization();
+		// OrganizationDTO top = OrganizationDTO.generateDtoBy(organization);
+		// findChildrenOfOrganization(top, organization);
+
+		OrganizationDTO top = _getOrganizationTree();
 		return top;
 	}
 
 	/**
 	 * 递归查询顶级机构的所有层次的子机构
+	 * 
 	 * @param organizationDTO
 	 * @param organization
 	 */
@@ -99,6 +106,46 @@ public class OrganizationApplicationImpl implements OrganizationApplication {
 	public OrganizationDTO getOrganizationById(Long id) {
 		Organization organization = OrganizationAbstractEntity.get(Organization.class, id);
 		return organization == null ? null : OrganizationDTO.generateDtoBy(organization);
+	}
+
+	public OrganizationDTO _getOrganizationTree() {
+		StringBuilder jpql = new StringBuilder("SELECT NEW org.openkoala.organisation.application.dto.OrganizationDTO"
+				+ "(r.id,olm.commissioner.id, r.name, r.sn,r.createDate, r.description, r.category) "
+				+ "FROM OrganizationLineManagement olm LEFT JOIN olm.responsible r "
+				+ "WHERE olm.commissioner is null AND olm.toDate > :queryDate AND olm.fromDate <= :queryDate");
+		QueryChannelService queryChannel = InstanceFactory.getInstance(QueryChannelService.class, "queryChannel_org");
+		OrganizationDTO top = (OrganizationDTO) queryChannel.createJpqlQuery(jpql.toString()).addParameter("queryDate", new Date()).singleResult();
+
+		jpql = new StringBuilder("SELECT NEW org.openkoala.organisation.application.dto.OrganizationDTO"
+				+ "(r.id,olm.commissioner.id, r.name, r.sn,r.createDate, r.description, r.category) "
+				+ "FROM OrganizationLineManagement olm LEFT JOIN olm.responsible r "
+				+ "WHERE olm.commissioner is not null AND  olm.toDate > :queryDate AND olm.fromDate <= :queryDate");
+		@SuppressWarnings("unchecked")
+		List<OrganizationDTO> all = queryChannel.createJpqlQuery(jpql.toString()).addParameter("queryDate", new Date()).list();
+		Map<Long, OrganizationDTO> map = new HashMap<Long, OrganizationDTO>();
+		for (OrganizationDTO organizationDTO : all) {
+			map.put(organizationDTO.getId(), organizationDTO);
+		}
+		map.put(top.getId(), top);
+		for (OrganizationDTO organizationDTO : map.values()) {
+			Long pid = organizationDTO.getPid();
+			if (pid == null || map.get(pid) == null) {
+				continue;
+			}
+			map.get(pid).getChildren().add(organizationDTO);
+		}
+
+		// _findChildrenOfOrganization(top, all);
+		return top;
+	}
+
+	private void _findChildrenOfOrganization(OrganizationDTO parent, List<OrganizationDTO> all) {
+		for (OrganizationDTO organizationDTO : all) {
+			if (organizationDTO.getPid() == parent.getId()) {
+				parent.getChildren().add(organizationDTO);
+				_findChildrenOfOrganization(organizationDTO, all);
+			}
+		}
 	}
 
 }
