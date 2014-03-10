50151252d220f230c03ba255b30e180f362a2d89
lingen.liu <lingen.liu@gmail.com>
2014-03-10 21:06:33 +0800

    nothing

--- koala-businesslog/koala-businesslog-impl/src/test/resources/BusinesslogConfig.groovy
+++ koala-businesslog/koala-businesslog-impl/src/test/resources/BusinesslogConfig.groovy
@@ -13,6 +13,7 @@ class BusinesslogConfig {
         String xx = contractApplication.addContract(22)
 
         "add invoice " + xx
+        
     }
 
     def ProjectApplicationImpl_findSomeProjects() {
