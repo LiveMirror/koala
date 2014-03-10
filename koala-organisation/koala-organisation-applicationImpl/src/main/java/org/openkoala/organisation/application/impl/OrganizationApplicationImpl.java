531bc9c2caccfb9da857e889b57f6d7853edd88a
zhaoyuxin <zhaoyuxin@foreveross.com>
2014-03-10 11:22:08 +0800

    把map改成LinkedHashMap,解决排序的问题

--- koala-organisation/koala-organisation-applicationImpl/src/main/java/org/openkoala/organisation/application/impl/OrganizationApplicationImpl.java
+++ koala-organisation/koala-organisation-applicationImpl/src/main/java/org/openkoala/organisation/application/impl/OrganizationApplicationImpl.java
@@ -1,9 +1,8 @@
 package org.openkoala.organisation.application.impl;
 
 import java.util.Date;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.ejb.Remote;
@@ -122,7 +121,7 @@ public class OrganizationApplicationImpl implements OrganizationApplication {
 				+ "WHERE olm.commissioner is not null AND  olm.toDate > :queryDate AND olm.fromDate <= :queryDate ORDER BY r.id ASC");
 		@SuppressWarnings("unchecked")
 		List<OrganizationDTO> all = queryChannel.createJpqlQuery(jpql.toString()).addParameter("queryDate", new Date()).list();
-		Map<Long, OrganizationDTO> map = new HashMap<Long, OrganizationDTO>();
+		LinkedHashMap<Long, OrganizationDTO> map = new LinkedHashMap<Long, OrganizationDTO>();
 		for (OrganizationDTO organizationDTO : all) {
 			map.put(organizationDTO.getId(), organizationDTO);
 		}
