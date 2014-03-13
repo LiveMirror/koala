ca8f89265d7cf2e8a03b740bf65b0d148ad08974
zhaoyuxin <zhaoyuxin@foreveross.com>
2014-03-13 17:04:25 +0800

    修正无法删除组织机构的问题

--- koala-organisation/koala-organisation-web/src/main/webapp/js/organisation/department.js
+++ koala-organisation/koala-organisation-web/src/main/webapp/js/organisation/department.js
@@ -45,7 +45,7 @@ var department = function(){
 	var del = function(org, type, $element){
         delete org.title;
 		var url = 'terminate-department.koala';
-		if(type == 'company'){
+		if(type == 'Company'){
 			url = 'terminate-company.koala';
 		}
 		$.post(baseUrl + url, org).done(function(data){
@@ -266,7 +266,7 @@ var department = function(){
             var zNodes = new Array();
             $.each(data, function(){
                 var zNode = {};
-                if(this.organizationType == 'company'){
+                if(this.organizationType == 'Company'){
                     zNode.type = 'parent';
                 }else{
                     zNode.icon = 'glyphicon glyphicon-list-alt'
@@ -313,7 +313,7 @@ var department = function(){
                     'update': function(event, data){
                         var $element = $(data);
                         var data = $element.data();
-                        if(data.organizationType == 'company'){
+                        if(data.organizationType == 'Company'){
                             updateCompany(data.id, $element);
                         }else{
                             updateDepartment(data.id, $element);
@@ -347,6 +347,7 @@ var department = function(){
 	};
     var createRightMenu = function(ev){
         var $element = $(ev.currentTarget);
+        alert($element.data('organizationType'));
         var menuData = null;
         menuData = [
             {title:'创建分公司', action: 'addCompany'},
@@ -354,7 +355,7 @@ var department = function(){
             {title:'修改机构信息', action: 'update'},
             {title:'撤销', action: 'delete'}
         ];
-        if($element.data('organizationType') == 'company'){
+        if($element.data('organizationType') == 'Company'){
             menuData = [
                 {title:'创建分公司', action: 'addCompany'},
                 {title:'创建下级部门', action: 'addDepartment'},
@@ -374,7 +375,7 @@ var department = function(){
     var getChildrenData = function(nodes, items){
         $.each(items, function(){
             var zNode = {};
-            if(this.organizationType == 'company'){
+            if(this.organizationType == 'Company'){
                 zNode.type = 'parent';
             }else{
                 zNode.icon = 'glyphicon glyphicon-list-alt'
@@ -400,7 +401,7 @@ var department = function(){
 			departmentDetail.find('[data-role="principalName"]').html(org.principalName);
 			departmentDetail.find('[data-role="organizationType"]').val(org.organizationType);
 			//loadEmployeeList(org.id);
-			if(org.organizationType == 'company'){
+			if(org.organizationType == 'Company'){
 				$('#addCompany').show();
 				$('#updateCompany').show();
 				$('#updateDepartment').hide();
