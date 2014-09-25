0e1106f884c211fd02118df1a8b9fe74629343af
luzhao <llzqingdao2012@163.com>
2014-09-25 15:46:56 +0800

    删除插件中的dataFilter

--- koala-security-org/koala-security-org-web/src/main/webapp/lib/koala-ui.plugin.js
+++ koala-security-org/koala-security-org-web/src/main/webapp/lib/koala-ui.plugin.js
@@ -339,11 +339,6 @@
 				dataType : 'json',
 				success : function(result){
 
-                    if(self.options.dataFilter){
-                        result = self.options.dataFilter(result);
-                        if(result.false === false) return;
-                    }
-
 					if (!result.data) {
 						self.$element.message({
 							type : 'error',
